72468e8b2dc4e5edc066eeba951c9accee43e4c4
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-28 18:17:42 -0800

    attained an api key, put it in my code

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function) {
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCm2k5RZ891YjEP8z1nTWYGFs2Ui8Zdadw",
+    authDomain: "train-schedule-d1da5.firebaseapp.com",
+    databaseURL: "https://train-schedule-d1da5.firebaseio.com",
+    projectId: "train-schedule-d1da5",
+    storageBucket: "train-schedule-d1da5.appspot.com",
+    messagingSenderId: "997545934192"
+  };
+  firebase.initializeApp(config);
+</script>
+}
+console.log(firebase);
