1b73722c42dc9a61e1cb52a8a74142bc6b47175e
Fernando Mendoza <fern.mendoza13@gmail.com>
2018-11-28 20:16:54 -0800

    added a click fucnction for adding train button

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,36 @@
-$(document).ready(function) {
-
+$(document).ready(function() {
+// Firebase link
 <script>
-  // Initialize Firebase
+
   var config = {
     apiKey: "AIzaSyCm2k5RZ891YjEP8z1nTWYGFs2Ui8Zdadw",
     authDomain: "train-schedule-d1da5.firebaseapp.com",
     databaseURL: "https://train-schedule-d1da5.firebaseio.com",
-    projectId: "train-schedule-d1da5",
+    pro jectId: "train-schedule-d1da5",
     storageBucket: "train-schedule-d1da5.appspot.com",
     messagingSenderId: "997545934192"
   };
   firebase.initializeApp(config);
 </script>
-}
+
 console.log(firebase);
+
+var database = firebase.database();
+
+// Train button
+$("#addTrainBtn").on("click", function(){
+
+    var trainName = $("trainNameInput").val.trim();
+    var lineName = $("#lineInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var trainTimeInput = moment($("#trainTimeInput").val().trim(), "HH:mm").subtract(10, "years").format("X");;
+    var frequencyInput = $("#frequencyInput").val().trim();
+
+    console.log(trainName);
+    console.log(lineName);
+    console.log(destination);
+    console.log(trainTimeInput);
+    console.log(frequencyInput);
+})
+
+});
